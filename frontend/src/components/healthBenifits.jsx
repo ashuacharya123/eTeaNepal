3d35748de5483fc3b6b1c9c163097c8b969eca70
ashuacharya <ashuacharya622@gmail.com>
2024-09-11 22:35:05 +0545

    removed healthBenifit animation

--- frontend/src/components/healthBenifits.jsx
+++ frontend/src/components/healthBenifits.jsx
@@ -3,13 +3,13 @@ import health from "../Assets/icons/Heathbenifits.svg";
 import health2 from "../Assets/icons/Healthbenifuts2.svg";
 
 const HealthBenifits = () => {
-  const [move, setMove] = useState(); //[44,54,64,74]
-  document.addEventListener("scroll", () => {
-    const maxHeight = document.body.scrollHeight - window.innerHeight;
-    setMove((window.pageYOffset * 100) / maxHeight);
-  });
+  // const [move, setMove] = useState(); //[44,54,64,74]
+  // document.addEventListener("scroll", () => {
+  //   const maxHeight = document.body.scrollHeight - window.innerHeight;
+  //   setMove((window.pageYOffset * 100) / maxHeight);
+  // });
 
-  const yOffset = 33;
+  // const yOffset = 33;
   return (
     <div className="health__container">
       <div className="health__container__content">
@@ -18,7 +18,7 @@ const HealthBenifits = () => {
         </div>
         <div
           className="health__container__content__health"
-          id={move > yOffset ? "card__animation" : ""}
+          // id={move > yOffset ? "card__animation" : ""}
         >
           <img
             src={window.visualViewport.width > 756 ? health2 : health}
